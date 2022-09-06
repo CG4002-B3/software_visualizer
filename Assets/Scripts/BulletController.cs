7e0a452cb02b038e0cdde39ee4188cb57479f197
tienkhoa16 <nguyentienkhoa22@gmail.com>
2022-09-06 20:33:53 +0800

    Add reloading animation

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,30 +6,62 @@ using UnityEngine.UI;
 
 public class BulletController : MonoBehaviour
 {
+    private int MAX_NUM_OF_BULLETS = 6;
     private float RELOAD_TIME = 1f;
 
     private Text bullets;
     private int bulletsRemaining;
 
+    public Animator animator;
+
+    private bool isReloading = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        bulletsRemaining = 6;
+        bulletsRemaining = MAX_NUM_OF_BULLETS;
         bullets = GetComponent<Text>();
+    }
+
+    void OnEnable()
+    {
+        animator.SetBool("Reloading", false);
+        isReloading = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bullets.text = bulletsRemaining.ToString() + " / 6";
+        if (isReloading)
+        {
+            return;
+        }
+
+        if (bulletsRemaining <= -1)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        bullets.text = bulletsRemaining.ToString() + " / " + MAX_NUM_OF_BULLETS;
     }
 
     public void ReduceBullets()
     {
-        bulletsRemaining = Math.Max(bulletsRemaining - 1, -1);
-        if (bulletsRemaining == -1)
+        if (isReloading)
         {
-            bulletsRemaining = 6;
+            return;
         }
+        bulletsRemaining = Math.Max(bulletsRemaining - 1, -1);
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        animator.SetBool("Reloading", true);
+        yield return new WaitForSeconds(RELOAD_TIME);
+        animator.SetBool("Reloading", false);
+        bulletsRemaining = MAX_NUM_OF_BULLETS;
+        isReloading = false;
     }
 }

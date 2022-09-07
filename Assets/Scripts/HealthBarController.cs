391f4d8286f8c8dc80c917295a30a7b158520851
tienkhoa16 <nguyentienkhoa22@gmail.com>
2022-09-08 04:26:54 +0800

    Add health bar ui + reduce logic

---
+++ Assets/Scripts/HealthBarController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    private int MAX_HEALTH = 100;
+
+    public int healthRemaining;
+    public Image[] healthSegments;
+    public Text healthPointText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthRemaining = MAX_HEALTH;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthPointText.text = healthRemaining.ToString();
+
+        for (int i = 0; i < healthSegments.Length; i++)
+        {
+            if (i < (int)(healthRemaining / 10))
+            {
+                healthSegments[i].enabled = true;
+            }
+            else
+            {
+                healthSegments[i].enabled = false;
+            }
+        }
+    }
+
+    public void ReduceHealth()
+    {
+        Debug.Log("Reducingggg");
+        healthRemaining = Math.Max(healthRemaining - 10, 0);
+        if (healthRemaining == 0)
+        {
+            healthRemaining = MAX_HEALTH;
+        }
+    }
+}

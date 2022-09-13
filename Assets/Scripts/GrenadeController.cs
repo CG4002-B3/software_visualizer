2b5a965a8a9f9e893d87ae9b92426aad5755637a
tienkhoa16 <nguyentienkhoa22@gmail.com>
2022-09-14 03:24:12 +0800

    Add check whether grenade hit based on target found

--- Assets/Scripts/GrenadeController.cs
+++ Assets/Scripts/GrenadeController.cs
@@ -9,11 +9,18 @@ public class GrenadeController : MonoBehaviour
     private int MAX_NUM_OF_GRENADES = 2;
     private int GRENADE_DAMAGE = 30;
 
+    private bool isOppFound;
+
     public int grenadesRemaining;
     public Image[] grenades;
     public ExplosionController explosionController;
     public OppHealthBarController oppHealthBarController;
     public OppShieldController oppshieldController;
+
+    void Start()
+    {
+        isOppFound = false;
+    }
 
     void Update()
     {
@@ -35,13 +42,22 @@ public class GrenadeController : MonoBehaviour
         if (!explosionController.GetIsGrenadeThrown())
         {
             grenadesRemaining = Math.Max(grenadesRemaining - 1, 0);
-            oppshieldController.ReduceShieldHp(GRENADE_DAMAGE);
-            oppHealthBarController.ReduceHealth(GRENADE_DAMAGE);
+            if (isOppFound)
+            {
+                oppshieldController.ReduceShieldHp(GRENADE_DAMAGE);
+                oppHealthBarController.ReduceHealth(GRENADE_DAMAGE);
+            }
         }
     }
 
     public void ResetGrenadesRemaining()
     {
         grenadesRemaining = MAX_NUM_OF_GRENADES;
+    }
+
+
+    public void SetIsOppFound(bool oppFoundValue)
+    {
+        isOppFound = oppFoundValue;
     }
 }

09844141feddb89ff1117279d2e042ba5a470bb4
tienkhoa16 <nguyentienkhoa22@gmail.com>
2022-10-05 13:27:14 +0800

    Add sample MQTT file

---
+++ Assets/Scripts/ShieldController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldController : MonoBehaviour
+{
+    private int MAX_NUM_OF_SHIELDS = 3;
+
+    public int shieldsRemaining;
+    public Image[] shields;
+    public ShieldOverlayController shieldOverlayController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < shields.Length; i++)
+        {
+            if (i < shieldsRemaining)
+            {
+                shields[i].enabled = true;
+            }
+            else
+            {
+                shields[i].enabled = false;
+            }
+        }
+    }
+
+    public void ReduceShield()
+    {
+        if (!shieldOverlayController.GetIsShowingShield())
+        {
+            shieldsRemaining = Math.Max(shieldsRemaining - 1, 0);
+        }
+    }
+
+    public void ResetShieldsRemaining()
+    {
+        shieldsRemaining = MAX_NUM_OF_SHIELDS;
+    }
+}

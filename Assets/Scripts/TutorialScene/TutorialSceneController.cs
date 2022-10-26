02e19319b05cf394454d39717722423f8d78dc28
tienkhoa16 <nguyentienkhoa22@gmail.com>
2022-10-27 00:50:29 +0800

    Darken background when display video

--- Assets/Scripts/TutorialScene/TutorialSceneController.cs
+++ Assets/Scripts/TutorialScene/TutorialSceneController.cs
@@ -11,6 +11,7 @@ public class TutorialSceneController : MonoBehaviour
     public Text narratorWords;
     public VideoPlayer shieldTutorial;
     public RawImage videoTexture;
+    public Image backgroundImage;
 
     private float NARRATOR_ANIMATOR_DURATION = 3f;
     private float NARRATOR_TRANSITION_OFFSET = 0.5f;
@@ -60,19 +61,59 @@ public class TutorialSceneController : MonoBehaviour
         welcomeMsg = false;
         showingMsg = false;
 
+        wordFadingEffect.SetBool("showNarrator", false);
         showTutorial1 = true;
     }
 
     IEnumerator playTutorial1()
     {
-        videoTexture.enabled = true;
         showingMsg = true;
-        shieldTutorial.Play();
 
+        narratorWords.text = "Here's how to defense yourself out there";
+        wordFadingEffect.SetBool("showNarrator", true);
+        yield return new WaitForSeconds(NARRATOR_ANIMATOR_DURATION);
+        wordFadingEffect.SetBool("showNarrator", false);
+        yield return new WaitForSeconds(NARRATOR_TRANSITION_OFFSET);
+
+        DarkenBackground();
+        videoTexture.enabled = true;
+        shieldTutorial.Play();
         yield return new WaitForSeconds((float)shieldTutorial.length);
+        LightenBackground();
 
         videoTexture.enabled = false;
         showTutorial1 = false;
         showingMsg = false;
+    }
+
+    IEnumerator playTutorial2()
+    {
+        showingMsg = true;
+
+        narratorWords.text = "Here's how to defense yourself out there";
+        wordFadingEffect.SetBool("showNarrator", true);
+        yield return new WaitForSeconds(NARRATOR_ANIMATOR_DURATION);
+        wordFadingEffect.SetBool("showNarrator", false);
+        yield return new WaitForSeconds(NARRATOR_TRANSITION_OFFSET);
+
+        DarkenBackground();
+        videoTexture.enabled = true;
+        shieldTutorial.Play();
+        yield return new WaitForSeconds((float)shieldTutorial.length);
+        LightenBackground();
+
+        videoTexture.enabled = false;
+        showTutorial1 = false;
+        showingMsg = false;
+    }
+
+    private void DarkenBackground()
+    {
+        backgroundImage.color = new Color(0f, 0f, 0f, 1f);
+    }
+
+    private void LightenBackground()
+    {
+        backgroundImage.color = new Color(0.76470588235f, 0.06666666666f, 0.61568627451f, 1f);
     }
 }

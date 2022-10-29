c2f55741208bbc6298a335412106d6d0e75d5d2c
tienkhoa16 <nguyentienkhoa22@gmail.com>
2022-10-30 01:13:37 +0800

    Fix video playing on phone

--- Assets/Scripts/TutorialScene/TutorialSceneController.cs
+++ Assets/Scripts/TutorialScene/TutorialSceneController.cs
@@ -19,6 +19,7 @@ public class TutorialSceneController : MonoBehaviour
     private bool showingMsg = false;
     private bool welcomeMsg = true;
     private bool showTutorial1 = false;
+    private bool showTutorial2 = false;
 
     void Start()
     {
@@ -79,33 +80,42 @@ public class TutorialSceneController : MonoBehaviour
         videoTexture.enabled = true;
         shieldTutorial.Play();
         yield return new WaitForSeconds((float)shieldTutorial.length);
-        LightenBackground();
-
-        videoTexture.enabled = false;
-        showTutorial1 = false;
-        showingMsg = false;
-    }
-
-    IEnumerator playTutorial2()
-    {
-        showingMsg = true;
-
-        narratorWords.text = "Here's how to defense yourself out there";
-        wordFadingEffect.SetBool("showNarrator", true);
-        yield return new WaitForSeconds(NARRATOR_ANIMATOR_DURATION);
-        wordFadingEffect.SetBool("showNarrator", false);
         yield return new WaitForSeconds(NARRATOR_TRANSITION_OFFSET);
 
-        DarkenBackground();
-        videoTexture.enabled = true;
-        shieldTutorial.Play();
-        yield return new WaitForSeconds((float)shieldTutorial.length);
-        LightenBackground();
+        TransparentBackground();
 
         videoTexture.enabled = false;
+
+        narratorWords.text = "Now it's your turn to shield";
+        wordFadingEffect.SetBool("showNarrator", true);
+        yield return new WaitForSeconds(NARRATOR_ANIMATOR_DURATION - 0.2f);
+        wordFadingEffect.SetBool("showNarrator", false);
+        // yield return new WaitForSeconds(NARRATOR_TRANSITION_OFFSET);
+
         showTutorial1 = false;
         showingMsg = false;
     }
+
+    // IEnumerator playTutorial2()
+    // {
+    //     showingMsg = true;
+
+    //     narratorWords.text = "Here's how to defense yourself out there";
+    //     wordFadingEffect.SetBool("showNarrator", true);
+    //     yield return new WaitForSeconds(NARRATOR_ANIMATOR_DURATION);
+    //     wordFadingEffect.SetBool("showNarrator", false);
+    //     yield return new WaitForSeconds(NARRATOR_TRANSITION_OFFSET);
+
+    //     DarkenBackground();
+    //     videoTexture.enabled = true;
+    //     shieldTutorial.Play();
+    //     yield return new WaitForSeconds((float)shieldTutorial.length);
+    //     LightenBackground();
+
+    //     videoTexture.enabled = false;
+    //     showTutorial2 = false;
+    //     showingMsg = false;
+    // }
 
     private void DarkenBackground()
     {
@@ -115,5 +125,10 @@ public class TutorialSceneController : MonoBehaviour
     private void LightenBackground()
     {
         backgroundImage.color = new Color(0.76470588235f, 0.06666666666f, 0.61568627451f, 1f);
+    }
+
+    private void TransparentBackground()
+    {
+        backgroundImage.color = new Color(0f, 0f, 0f, 0f);
     }
 }
